04dcf9711980d7ad8fbdaf4892f9f7f5e876a072
Birkan Karakaya <birkan.karakayaa@gmail.com>
2023-07-29 17:46:26 +0300

    Create "ConfirmCode" for mail confirm and send confirm mail

--- PresentationLayer/Controllers/RegisterController.cs
+++ PresentationLayer/Controllers/RegisterController.cs
@@ -1,7 +1,11 @@
 ﻿using DtoLayer.Dtos.AppUserDtos;
 using EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System.Net.Mail;
+
 
 namespace PresentationLayer.Controllers
 {
@@ -25,19 +29,44 @@ namespace PresentationLayer.Controllers
         {
             if (ModelState.IsValid)
             {
-                AppUser appUser = new AppUser()
+                Random random = new Random();
+                int code = random.Next(100000, 1000000);
+
+				AppUser appUser = new AppUser()
                 {
                     UserName = appUserRegisterDto.Username,
                     Name = appUserRegisterDto.Name,
                     Surname = appUserRegisterDto.Surname,
-                    Email = appUserRegisterDto.Email
+                    Email = appUserRegisterDto.Email,
+                    ConfirmCode = code
                 };
 
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "ConfirmMail");
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("Easy Cash Admin", "MailAdress");
+                    MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+                    mimeMessage.From.Add(mailboxAddressFrom);
+                    mimeMessage.To.Add(mailboxAddressTo);
+
+                    var bodyBuilder = new BodyBuilder();
+                    bodyBuilder.TextBody = "Kayıt işlemini gerçekletirmek için onay kodunuz: " + code;
+                    mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+                    mimeMessage.Subject = "Easy Cash Onay Kodu";
+
+                    MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient();
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("MailAdress", "Password");
+                    client.Send(mimeMessage);
+                    client.Disconnect(true);
+
+
+
+					return RedirectToAction("Index", "ConfirmMail");
                 }
 
                 else
